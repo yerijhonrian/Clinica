dd29489203e69fd1261fb74a970628f6d959def1
contactJoel <contato.yerijhonrian@gmail.com>
2026-01-03 15:12:52 -0300

    ClinicaOdontológica

---
+++ script.js
@@ -0,0 +1,31 @@
+const openForm = document.getElementById('openForm');
+    const modal = document.getElementById('modalForm');
+    const closeForm = document.getElementById('closeForm');
+
+    openForm.addEventListener('click', () => {
+        modal.classList.add('active');
+    });
+
+    closeForm.addEventListener('click', () => {
+        modal.classList.remove('active');
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove('active');
+        }
+    });
+
+    const animatedElements = document.querySelectorAll('.animate');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+animatedElements.forEach(el => observer.observe(el));
